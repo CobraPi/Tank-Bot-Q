975ac0d34550e391f92375f9ffc88eeb30d4f19d
Joey Orduna <85652317+Joey-Orduna@users.noreply.github.com>
2023-06-02 22:08:55 -0400

    radio working now

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
-#include <Arduino_due_FreeRTOS.h>
 #include <RF24.h>
+
+#define PIN_R_CW   2
+#define PIN_R_CCW  3
+#define PIN_R_EN   9
+#define PIN_L_CW   4
+#define PIN_L_CCW  5
+#define PIN_L_EN   10
 
 // These are the different control modes
 enum ControlMode {
@@ -11,16 +17,17 @@ enum ControlMode {
 
 // This is the structure of the received data packet
 typedef struct {
-  uint8_t radioId;  
-  uint8_t adcX;
-  uint8_t adcY;
-  bool middleButton;
+  //uint8_t radioId;  
+  int16_t adcX;
+  int16_t adcY;
+  bool buttonXy;
   bool buttonA;
   bool buttonB;
   bool buttonC;
   bool buttonD;
+  bool buttonE;
+  bool buttonF;
 } rx_radio_data_t;
-
 // This is the structure of the transmitted data packet
 typedef struct {
   uint8_t radioId; 
@@ -35,47 +42,79 @@ const static uint8_t RADIO_ID = 0x01;
 
 rx_radio_data_t rx_radio_data;
 tx_radio_data_t tx_radio_data;
-RF24 radio(7,8);
-TaskHandle_t txData;
+RF24 radio(11,12);
 
 static void init_radio() {
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.openWritingPipe(RADIO_ID);
+  //rx_radio_data.radioId = RADIO_ID;
+  while(!radio.begin()) Serial.println("Radio hardware not responding");  
+  radio.setPALevel(RF24_PA_LOW);
+  //radio.openWritingPipe(RADIO_ID);
   radio.openReadingPipe(RADIO_ID, DESTINATION_RADIO_ID);
   radio.startListening(); 
 }
 
 // Receive and transmit RF data every 10 ms 
-static void TaskRxTxRFData(void* pvParameters) {
-  byte pipe;
-  while(1) { 
+static void TaskRxTxRFData() {
+    byte pipe;
     if(radio.available(&pipe)) {
-      Serial.println("Got data"); 
-      radio.read(&rx_radio_data, sizeof(rx_radio_data));
-      radio.stopListening();
-      radio.write(&tx_radio_data, sizeof(tx_radio_data));
-      radio.startListening(); 
+      radio.read(&rx_radio_data, sizeof(rx_radio_data_t));
+      int16_t adcX = rx_radio_data.adcX;
+      int16_t adcY = rx_radio_data.adcY; 
+      Serial.print(adcX); Serial.print(" | ");
+      Serial.println(adcY);  
     }
-    vTaskDelay(10 / portTICK_PERIOD_MS);
+}
+
+void set_l_pwm(int16_t pwm) {
+  if(pwm < 0) {
+    analogWrite(PIN_L_CW, abs(pwm));
+    digitalWrite(PIN_L_CCW, LOW);
+  } 
+  else {
+    analogWrite(PIN_L_CCW, pwm);
+    digitalWrite(PIN_L_CW, LOW);
   }
 }
 
+void set_r_pwm(int16_t pwm) {
+  if(pwm < 0) {
+    analogWrite(PIN_R_CW, abs(pwm));
+    digitalWrite(PIN_R_CCW, LOW);
+  } 
+  else {
+    analogWrite(PIN_R_CCW, pwm);
+    digitalWrite(PIN_R_CW, LOW);
+  }
+
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(250000);
+  pinMode(13, OUTPUT);
+  pinMode(PIN_L_CW, OUTPUT);
+  pinMode(PIN_L_CCW, OUTPUT);
+  pinMode(PIN_R_CW, OUTPUT);
+  pinMode(PIN_R_CCW, OUTPUT); 
+  pinMode(PIN_L_EN, OUTPUT);
+  pinMode(PIN_R_EN, OUTPUT);
+  digitalWrite(PIN_L_EN, HIGH);
+  digitalWrite(PIN_R_EN, HIGH);
   init_radio(); 
-  portBASE_TYPE rx, tx;
-  Serial.println("Radio initialized"); 
-  tx = xTaskCreate(TaskRxTxRFData, NULL, configMINIMAL_STACK_SIZE, NULL, 1, &txData);
-  if(tx != pdPASS) {
-    Serial.println(F("Error creating tasks"));
-  }
-  vTaskStartScheduler();
-  Serial.println("Insufficient RAM");
-  while(1);
+  analogWriteResolution(12);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  TaskRxTxRFData();
+  if(rx_radio_data.buttonXy) 
+    digitalWrite(13, HIGH);
+  else 
+    digitalWrite(13, LOW);
+  int16_t adcX = rx_radio_data.adcX;
+  int16_t adcY = rx_radio_data.adcY;
+  int16_t lPwm = map(adcX + adcY, -500, 500, -4095, 4095);
+  int16_t rPwm = map(adcX - adcY, -500, 500, -4095, 4095);
+  set_l_pwm(lPwm);
+  set_r_pwm(rPwm);
 }
